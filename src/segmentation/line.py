e7a5a8dae9393f6e05f66d8d363ed0f97bdf51e9
Georg Groenendaal <georg.groenendaal@gmail.com>
2022-05-06 10:35:12 +0000

    add joblib caching

--- src/segmentation/line.py
+++ src/segmentation/line.py
@@ -1,9 +1,16 @@
+from functools import cached_property
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.typing as npt
 from scipy import ndimage
 
 from src.utils.images import get_name, store_image
 from src.utils.logger import logger
+from src.utils.cache import memory
+
+
+GrayScaleImage = npt.NDArray[np.uint8]
+TraceMap = npt.NDArray[np.bool_]
 
 
 class LineSegmenter:
@@ -19,22 +26,29 @@ class LineSegmenter:
     def segment_lines(self, image_path: str) -> None:
         name = get_name(image_path)
 
-        image = plt.imread(image_path)
+        image: GrayScaleImage = plt.imread(image_path)
         binarized_image = image < self.binary_cutoff
 
+        # component labeling and height finding
         components, n_components = ndimage.label(binarized_image)
         heights = self._component_heights(components, n_components)
-        mean_heights = sum(heights) / len(heights)
+        mean_component_height = sum(heights) / len(heights)
+        blurred_width = int(mean_component_height * 8)
+        blurred_height = int(mean_component_height * 0.8)
 
-        kernel_x = mean_heights * 8
-        kernel_y = mean_heights * 0.8
-
-        blurred_image = ndimage.uniform_filter(image, (kernel_y, kernel_x))
+        cached_blur = memory.cache(self._blur)
+        blurred_image: GrayScaleImage = cached_blur(
+            image, blurred_width, blurred_height
+        )
 
         if self.save_intermediate:
-            logger.debug(f"Storing intermediate {name}")
-
+            logger.debug(f"Storing intermediate blurred image {name}")
             store_image(blurred_image, f"data/intermediate/blurred/{name}.jpg", "gray")
+
+    def _blur(
+        self, image: GrayScaleImage, blurred_width: int, blurred_height: int
+    ) -> GrayScaleImage:
+        return ndimage.gaussian_filter(image, (blurred_height, blurred_width))
 
     def _component_heights(
         self, components: np.ndarray, n_components: int
@@ -47,3 +61,8 @@ class LineSegmenter:
             heights.append(vertical_sum.max())
 
         return heights
+
+    def _trace(self, blurred_image: GrayScaleImage, blurred_height: int) -> TraceMap:
+        map = np.zeros_like(blurred_image, dtype=np.bool_)
+
+        return map
