0ec92220a9205b9d232fa0ef4c9b87ad7b39bf4a
Georg Groenendaal <georg.groenendaal@gmail.com>
2022-05-12 15:06:58 +0000

    add filtering

--- src/segmentation/line.py
+++ src/segmentation/line.py
@@ -28,12 +28,13 @@ class LineSegmenter:
         binarized_image = image < self.binary_cutoff
 
         # component labeling and height finding
-        components, n_components = ndimage.label(binarized_image)
-        heights = self._component_heights(components, n_components)
+        components, _ = ndimage.label(binarized_image)
+        heights = self._component_heights(components)
         mean_component_height = sum(heights) / len(heights)
         blurred_width = int(mean_component_height * 8)
         blurred_height = int(mean_component_height * 0.8)
 
+        # performing blur, reusing cache if possible
         cached_blur = memory.cache(self._blur)
         blurred_image: GrayScaleImage = cached_blur(
             image, blurred_width, blurred_height
@@ -44,31 +45,54 @@ class LineSegmenter:
             store_image(blurred_image, f"data/intermediate/blurred/{name}.jpg", "gray")
 
         tracers = self._trace(blurred_image, blurred_height)
-        trace_image = self._trace_to_image(tracers)
+        trace_mask_rl = self._trace_to_mask(tracers)
 
-        blurred_image_inverted = self._horizontal_flip(blurred_image)
-        tracers_inverted = self._trace(blurred_image_inverted, blurred_height)
-        trace_image_inverted = self._trace_to_image(tracers_inverted)
-        trace_image_2 = self._horizontal_flip(trace_image_inverted)
+        # repeating for right to left
+        blurred_image_flipped = self._horizontal_flip(blurred_image)
+        tracers_flipped = self._trace(blurred_image_flipped, blurred_height)
+        trace_mask_flipped = self._trace_to_mask(tracers_flipped)
+        trace_mask_lr = self._horizontal_flip(trace_mask_flipped)
+
+        trace_mask_combined = trace_mask_lr & trace_mask_rl
+
+        filtered_components = self._filter_component_by_area(
+            trace_mask_combined, line_height=mean_component_height
+        )
 
         store_image(
-            np.invert(trace_image) | np.invert(trace_image_2) | binarized_image,
+            np.invert(filtered_components) | binarized_image,
             f"data/intermediate/trace/{name}.png",
             "gray",
         )
+
+    def _filter_component_by_area(
+        self, mask: npt.NDArray[np.bool_], line_height: float
+    ) -> npt.NDArray[np.bool_]:
+
+        cutoff_area = line_height**2  # from the paper it is 2
+        cutoff_height = line_height * 2  # we added this
+        components, _ = ndimage.label(mask)
+
+        for loc in ndimage.find_objects(components):
+            single_component = components[loc]
+            area = np.count_nonzero(single_component)
+            height = np.max(np.count_nonzero(single_component, 0))
+
+            if area < cutoff_area or height < cutoff_height:
+                components[loc] = 0
+
+        return components > 0
 
     def _blur(
         self, image: GrayScaleImage, blurred_width: int, blurred_height: int
     ) -> GrayScaleImage:
         return ndimage.gaussian_filter(image, (blurred_height, blurred_width))
 
-    def _component_heights(
-        self, components: np.ndarray, n_components: int
-    ) -> list[int]:
+    def _component_heights(self, labeled_components: np.ndarray) -> list[int]:
         heights: list[int] = []
 
-        for i in range(1, n_components + 1):
-            single_component = components == i
+        for loc in ndimage.find_objects(labeled_components):
+            single_component = labeled_components[loc] != 0
             vertical_sum = single_component.sum(axis=0)
             heights.append(vertical_sum.max())
 
@@ -96,16 +120,16 @@ class LineSegmenter:
 
         return tracers
 
-    def _trace_to_image(self, tracers: Tracers) -> npt.NDArray[np.bool_]:
+    def _trace_to_mask(self, tracers: Tracers) -> npt.NDArray[np.bool_]:
         height, width = tracers.shape
-        image = np.zeros_like(tracers, dtype=np.bool_)
+        mask = np.zeros_like(tracers, dtype=np.bool_)
 
         for i in range(width):
             indices = np.arange(height)
             tracer_col = tracers[:, i]
-            image[:, i] = np.invert(np.isin(indices, tracer_col))
+            mask[:, i] = np.invert(np.isin(indices, tracer_col))
 
-        return image
+        return mask
 
     def _horizontal_flip(self, arr: np.ndarray) -> np.ndarray:
         return arr[:, ::-1]
