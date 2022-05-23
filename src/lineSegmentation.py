621ce43652662c82d86d07c725aa2ea778cccc16
mortaza <mortaza.a.y.l@gmail.com>
2022-05-23 19:03:57 +0000

    merge conflict

--- src/lineSegmentation.py
+++ src/lineSegmentation.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
 import numpy as np
 from scipy import ndimage
+import matplotlib.pyplot as plt
 
 np.random.seed(1)
 n = 10
@@ -13,32 +13,31 @@ l = 256
 # mask = (im > im.mean()).astype(np.float)
 # mask += 0.1 * im
 
-img = plt.imread("C:/Users/mw0121921/Downloads/image-data/image-data/P123-Fg002-R-C01-R01-binarized.jpg");
+img = plt.imread(
+    "C:/Users/mw0121921/Downloads/image-data/image-data/P123-Fg002-R-C01-R01-binarized.jpg"
+)
 
 hist, bin_edges = np.histogram(img, bins="auto")
-bin_centers = 0.5*(bin_edges[:-1] + bin_edges[1:])
+bin_centers = 0.5 * (bin_edges[:-1] + bin_edges[1:])
 
-horizontal_hist = img.shape[1] - np.sum(img,axis=1,keepdims=True)/255
+horizontal_hist = img.shape[1] - np.sum(img, axis=1, keepdims=True) / 255
 
-#binary_img = img > 0.5
+# binary_img = img > 0.5
 
-plt.figure(figsize=(11,4))
+plt.figure(figsize=(11, 4))
 
 plt.subplot(131)
 plt.imshow(img)
-plt.axis('off')
+plt.axis("off")
 plt.subplot(132)
-#plt.plot(bin_centers, hist)
-plt.hist(img,bin="auto")
-plt.axvline(0.5, color='r', ls='--')
-plt.text(0.57, 0.8, 'histogram', fontsize=20, transform = plt.gca().transAxes)
+# plt.plot(bin_centers, hist)
+plt.hist(img, bin="auto")
+plt.axvline(0.5, color="r", ls="--")
+plt.text(0.57, 0.8, "histogram", fontsize=20, transform=plt.gca().transAxes)
 plt.yticks([])
 plt.subplot(133)
-plt.imshow(img, cmap=plt.cm.gray, interpolation='nearest')
-plt.axis('off')
+plt.imshow(img, cmap=plt.cm.gray, interpolation="nearest")
+plt.axis("off")
 
 plt.subplots_adjust(wspace=0.02, hspace=0.3, top=1, bottom=0.1, left=0, right=1)
 plt.show()
-
-
-
